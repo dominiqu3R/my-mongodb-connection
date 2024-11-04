f272efe7eab6a9512193e2605e788d03c84b2438
dominiqu3R <142631255+dominiqu3R@users.noreply.github.com>
2024-11-04 20:34:39 +0000

    working server

--- manipulateData.js
+++ manipulateData.js
@@ -7,27 +7,11 @@ const jsonFilePath = path.join(__dirname, 'foundDocuments.json');
 // Initialize the documents variable
 let documents = [];
 
-// Read the JSON file synchronously
-try {
-    const data = fs.readFileSync(jsonFilePath, 'utf8');
-    const trimmedData = data.trim();
-    documents = JSON.parse(trimmedData);
-
-    if (documents.length > 0) {
-        console.log('First document:', documents[0]);
-    } else {
-        console.log('No documents found.');
-    }
-} catch (err) {
-    console.error('Error reading or parsing JSON file:', err);
-}
-
-// Function to log document keys
-function logDocumentKeys(doc) {
-    for (let key in doc) {
-        console.log(`${key}: ${doc[key]}`);
-    }
-}
+sharedEmitter.on('documentsFetched', (newDocuments) => {
+    documents = newDocuments;
+    // Automatically save to file when documents are updated
+    saveDocuments();
+});
 
 // Getter function to retrieve documents
 function getDocuments() {
@@ -39,10 +23,11 @@ function saveDocuments() {
     try {
         fs.writeFileSync(jsonFilePath, JSON.stringify(documents, null, 2), 'utf8');
         console.log('Documents successfully saved to JSON file.');
+        sharedEmitter.emit('documentsSaved', documents);
     } catch (err) {
         console.error('Error writing JSON file:', err);
     }
 }
 
 // Export the functions
-module.exports = { getDocuments, logDocumentKeys, saveDocuments };
+module.exports = { getDocuments, saveDocuments, documents };
---
+++ shared.js
@@ -0,0 +1,6 @@
+const EventEmitter = require('events');
+
+class SharedEmitter extends EventEmitter { }
+const sharedEmitter = new SharedEmitter();
+
+module.exports = sharedEmitter;
